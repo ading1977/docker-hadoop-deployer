398784de51c150fa7e46afe562c0fbf6a3e20057
Meng Ding <dingmeng@gmail.com>
2015-10-21 22:08:42 +0000

    automatic upgrade to latest docker image

--- runHadoop.sh
+++ runHadoop.sh
@@ -17,6 +17,33 @@ docker_hadoop_usage() {
   echo "Usage: runHadoop.sh (namenode | datanode | resourcemanager | nodemanager)"
 }
 
+docker_hadoop_upgrade_image() {
+  local IMAGE=${HADOOP_DOCKER_IMAGE}
+  local CID=$(docker ps | grep $IMAGE | grep $DAEMON | awk '{print $1}')
+
+  # Pull the latest image if available
+  docker pull $IMAGE
+
+  # Is the container running
+  if [ -z $CID ]; then
+    return 1
+  fi
+
+  LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+  RUNNING=`docker inspect --format "{{.Image}}" $CID`
+  echo "Latest:" $LATEST
+  echo "Running:" $RUNNING
+  if [ "$RUNNING" != "$LATEST" ]; then
+    echo "upgrading $DAEMON image"
+    docker stop $DAEMON
+    docker rm -f $DAEMON
+    return 1
+  else
+    echo "$DAEMON is running, and the image is up to date"
+    return 0
+  fi
+}
+
 docker_hadoop_expand_env() {
   DOCKER_ENVS=""
   for var in  ${!HADOOP_*}; do
@@ -25,7 +52,12 @@ docker_hadoop_expand_env() {
 }
 
 docker_run() {
-  DAEMON=$1
+
+  docker_hadoop_upgrade_image
+  if [ $? -eq 0 ]; then
+    return 0
+  fi
+
   docker run -d --name ${DAEMON} --net=host \
     -v ${HADOOP_LOG_DIR}:${HADOOP_LOG_DIR} \
     -v ${HADOOP_CONF_DIR}:${HADOOP_CONF_DIR} \
@@ -38,6 +70,7 @@ if [[ $# = 0 ]]; then
   docker_hadoop_usage
   exit 1
 fi
+
 
 docker_hadoop_expand_env
 
